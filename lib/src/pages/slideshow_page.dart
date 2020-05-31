872934e9def440fa237b75b1a65208eb59775fee
andresperezfc <felipe3apc@gmail.com>
2020-05-31 17:09:20 -0500

    11 - cambio de color con animacion

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -41,12 +41,13 @@ class _Dot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
-    return Container(
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
       width: 12,
       height: 12,
       margin: EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
-          color: (pageViewIndex == index)
+          color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5)
               ? const Color(0xff5abd8c)
               : const Color(0xffD6EEE2),
           shape: BoxShape.circle),
