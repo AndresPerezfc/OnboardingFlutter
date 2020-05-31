bdd7db9538b85d100a9dd1d850d26a9bdc500402
andresperezfc <felipe3apc@gmail.com>
2020-05-31 16:43:44 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'src/pages/slideshow_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Onboarding Samaria P',
+        home: SlideShowPage());
+  }
+}
